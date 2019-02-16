77737619bd7643f67d1ab8b331f7dc36c7bdaac2
Behdad Karimi <behdad.k7@gmail.com>
2019-02-16 14:58:15 +0330

    Similarity fun added

--- src/engin.py
+++ src/engin.py
@@ -1,8 +1,15 @@
 import math
 
+Coppola_Brando = [(5.0,5.0),(5.0,4.29)]
+Coppola_Dreyfuss = [(5.0, 1.07), (5.0, 0.63)]
+Really_liked = [(5.0,5.0),(5.0,5.0)]
+
 # points are pair of tuples
 def euclidean_distance(points):
     squared_diffs = [(point[0] - point[1]) ** 2 for point in points]
     summed_squared_diffs = sum(squared_diffs)
     distance = math.sqrt(summed_squared_diffs)
     return distance
+
+def similarity(points):
+    return 1/(1 + euclidean_distance(points))
