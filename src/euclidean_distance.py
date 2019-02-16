8e40cedddeca83b033f72a141a0e79b724e43788
Behdad Karimi <behdad.k7@gmail.com>
2019-02-16 13:13:01 +0330

    euclidean_distance fun

---
+++ src/euclidean_distance.py
@@ -0,0 +1,8 @@
+import math
+
+# points are pair of tuples
+def euclidean_distance(points):
+    squared_diffs = [(point[0] - point[1]) ** 2 for point in points]
+    summed_squared_diffs = sum(squared_diffs)
+    distance = math.sqrt(summed_squared_diffs)
+    return distance
